8b0e30a146411ea1360d4b09976be7e3af1ac214
Alhadis <gardnerjohng@gmail.com>
2018-12-06 22:34:12 +1100

    Fix descriptions and titles of package settings

---
+++ form-feeds.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const {CompositeDisposable} = require("atom");
+
+let disposables = null;
+
+module.exports = {
+	
+	// Initialise the package and register event handlers
+	activate(state = {}){
+		if(null !== disposables)
+			disposables.dispose();
+			
+		// Update CSS classes on <body> when these settings change
+		disposables = new CompositeDisposable();
+		disposables.add(atom.config.observe("form-feeds.placeholderStyle", type => {
+			const classes = document.body.classList;
+			classes.toggle("ff-style-border", "border" === type);
+			classes.toggle("ff-style-caret",  "caret"  === type);
+			classes.toggle("ff-style-none",   "none"   === type);
+		});
+	
+	
+	// Free-up memoery when deactivating package
+	deactivate(){
+		if(null !== disposables){
+			disposables.dispose();
+			disposables = null;
+		}
+	},
+};
