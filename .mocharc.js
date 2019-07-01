0f1e73c5d8cd54fe6c46de73aa460cccf1457498
Alhadis <gardnerjohng@gmail.com>
2019-07-01 23:04:30 +1000

    Upgrade atom-mocha to v2.2.0

--- .mocharc.js
+++ .mocharc.js
@@ -1,16 +1,9 @@
 "use strict";
 
-const isCI =
-	!!(process.env.CI
-	|| process.env.CI_NAME
-	|| process.env.BUILD_NUMBER
-	|| process.env.RUN_ID);
-
 module.exports = {
-	bail: !isCI,
+	bail: !AtomMocha.isCI,
 	slow: 15000,
 	require: [
 		"chai/register-should",
-		"mocha-when/register",
 	],
 };
